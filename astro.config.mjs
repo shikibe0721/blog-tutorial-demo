9cda4eb8921794fc461093723e9ea7b3a7c784e5
shikibe0721 <shikibe0721@gmail.com>
2026-08-19 22:21:17 +0800

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import preact from "@astrojs/preact";
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'hybrid',  // ← 新增这一行
+  //output: 'hybrid',   ← 新增这一行
   site: "https://example.com",
   integrations: [preact()],
 });
